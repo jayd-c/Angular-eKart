759c4398f90f9770518e6dab9bca470d180e6825
JayanDesilva <jaycdesilva@gmail.com>
2024-04-16 14:13:32 -0400

    Upto Directives

---
+++ src/app/product-list/search/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'search',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+ 
+  searchText:string = "Mens' wear";
+
+//   updateSearchText(event: any) {
+//       this.searchText = event.target.value;
+
+// }
+
+}
